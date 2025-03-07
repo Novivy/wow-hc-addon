01a41bd0dc073390cd0253ae95eb48f244a60ad9
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2025-03-07 10:39:12 +0100

    Fixed blocking bidding on auction house.

--- AchievementHelpers.lua
+++ AchievementHelpers.lua
@@ -17,10 +17,14 @@ local function hooksecurefunc(arg1, arg2, arg3)
     end
     local orig = arg1[arg2]
     arg1[arg2] = function(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20)
-        --WHC.DebugPrint("Original "..arg2)
+        if arg2 ~= "UnitPopup_OnUpdate" then
+            --WHC.DebugPrint("Original "..arg2)
+        end
         local x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20 = orig(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20)
 
-        --WHC.DebugPrint("Hook "..arg2)
+        if arg2 ~= "UnitPopup_OnUpdate" then
+            --WHC.DebugPrint("Hook "..arg2)
+        end
         arg3(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20)
 
         return x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20
@@ -170,6 +174,13 @@ timeIsMoneyEventListener:SetScript("OnEvent", function(self, event, addonName)
         return
     end
 
+    hooksecurefunc("AuctionFrameBrowse_Update", function()
+        if WhcAddonSettings.blockAuctionBuy == 1 then
+            BrowseBidButton:Disable()
+            BrowseBuyoutButton:Disable()
+        end
+    end)
+
     hooksecurefunc("AuctionFrameBid_Update", function()
         if WhcAddonSettings.blockAuctionBuy == 1 then
             BidBidButton:Disable()
@@ -185,7 +196,7 @@ function WHC.SetBlockAuctionBuy()
     if WhcAddonSettings.blockAuctionBuy == 1 then
         -- Block outgoing trade
         PlaceAuctionBid = function()
-            printAchievementInfo(timeIsMoneyLink, "Post items to the auction house is blocked.")
+            printAchievementInfo(timeIsMoneyLink, "Buying items from the auction house is blocked.")
         end
     end
 end
