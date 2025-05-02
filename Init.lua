4322691ee88fec95e79ccecad41ae1905cf91b03
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2025-05-02 21:22:14 +0200

    boilerplate code in place

--- Init.lua
+++ Init.lua
@@ -74,6 +74,9 @@ WHC:SetScript("OnEvent", function(self, event, addonName)
     WhcAchievementSettings.blockRidingSkill = WhcAchievementSettings.blockRidingSkill or 0
     WhcAchievementSettings.blockProfessions = WhcAchievementSettings.blockProfessions or 0
     WhcAchievementSettings.blockQuests = WhcAchievementSettings.blockQuests or 0
+    WhcAchievementSettings.onlyKillUndead = WhcAchievementSettings.onlyKillUndead or 0
+    WhcAchievementSettings.onlyKillDemons = WhcAchievementSettings.onlyKillDemons or 0
+    WhcAchievementSettings.onlyKillBoars = WhcAchievementSettings.onlyKillBoars or 0
 
     WHC.InitializeUI()
     WHC.InitializeMinimapIcon()
