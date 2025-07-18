dbc75f0655b9c3258726b9c66b0e9c8be17970d8
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2025-07-18 08:49:37 +0200

    renamed variable to a meaninful name

--- Death.lua
+++ Death.lua
@@ -1,10 +1,9 @@
-local function createUrlFrame()
-    -- Create the URL frame
-    local urlFrame = CreateFrame("Frame", "URLFrame", UIParent, RETAIL_BACKDROP)
-    urlFrame:SetWidth(300)
-    urlFrame:SetHeight(150)
-    urlFrame:SetPoint("TOP", UIParent, "TOP", 0, -128)
-    urlFrame:SetBackdrop({
+local function createAppealFrame()
+    local appealFrame = CreateFrame("Frame", "AppealFrame", UIParent, RETAIL_BACKDROP)
+    appealFrame:SetWidth(300)
+    appealFrame:SetHeight(150)
+    appealFrame:SetPoint("TOP", UIParent, "TOP", 0, -128)
+    appealFrame:SetBackdrop({
         bgFile = "Interface/RaidFrame/UI-RaidFrame-GroupBg",
         edgeFile = "Interface/DialogFrame/UI-DialogBox-Border",
         tile = true,
@@ -13,37 +12,37 @@ local function createUrlFrame()
         insets = { left = 11, right = 12, top = 12, bottom = 11 }
     })
 
-    urlFrame:SetFrameStrata("HIGH")
-    urlFrame:SetFrameLevel(10)
+    appealFrame:SetFrameStrata("HIGH")
+    appealFrame:SetFrameLevel(10)
 
     -- Title
-    local title = urlFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-    title:SetPoint("TOP", urlFrame, "TOP", 0, -20)
+    local title = appealFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+    title:SetPoint("TOP", appealFrame, "TOP", 0, -20)
     title:SetText("Copy and paste the following URL to your browser to appeal")
     title:SetWidth(200)
 
     -- URL input box
-    local urlEditBox = CreateFrame("EditBox", "URLInputBox", urlFrame)
-    urlEditBox:SetWidth(250)
-    urlEditBox:SetHeight(20)
-    urlEditBox:SetPoint("TOP", title, "BOTTOM", 0, -20)
-    urlEditBox:SetFontObject("ChatFontNormal")
-    urlEditBox:SetText("https://wow-hc.com/appeal")
-    urlEditBox:SetJustifyH("CENTER")
-    urlEditBox:SetAutoFocus(false)
-    urlEditBox:HighlightText()
-    urlEditBox:SetFocus()
-    urlEditBox:SetTextColor(1, 0.631, 0.317)
-    urlEditBox:SetScript("OnMouseDown", function(self)
-        urlEditBox:HighlightText()
-        urlEditBox:SetFocus()
+    local appealEditBox = CreateFrame("EditBox", "AppealInputBox", appealFrame)
+    appealEditBox:SetWidth(250)
+    appealEditBox:SetHeight(20)
+    appealEditBox:SetPoint("TOP", title, "BOTTOM", 0, -20)
+    appealEditBox:SetFontObject("ChatFontNormal")
+    appealEditBox:SetText("https://wow-hc.com/appeal")
+    appealEditBox:SetJustifyH("CENTER")
+    appealEditBox:SetAutoFocus(false)
+    appealEditBox:HighlightText()
+    appealEditBox:SetFocus()
+    appealEditBox:SetTextColor(1, 0.631, 0.317)
+    appealEditBox:SetScript("OnMouseDown", function(self)
+        appealEditBox:HighlightText()
+        appealEditBox:SetFocus()
     end)
 
     -- Create a container for the buttons to manage their positioning
-    local buttonContainer = CreateFrame("Frame", nil, urlFrame)
+    local buttonContainer = CreateFrame("Frame", nil, appealFrame)
     buttonContainer:SetWidth(250) -- Width enough to fit both buttons
     buttonContainer:SetHeight(30) -- Height of the buttons
-    buttonContainer:SetPoint("TOP", urlEditBox, "BOTTOM", 0, -20)
+    buttonContainer:SetPoint("TOP", appealEditBox, "BOTTOM", 0, -20)
 
     -- Cancel button
     local cancelButton = CreateFrame("Button", nil, buttonContainer, "UIPanelButtonTemplate")
@@ -52,17 +51,17 @@ local function createUrlFrame()
     cancelButton:SetPoint("CENTER", buttonContainer, "CENTER", 5, 0) -- Position relative to container's center
     cancelButton:SetText("Back")
     cancelButton:SetScript("OnClick", function()
-        urlFrame:Hide()
+        appealFrame:Hide()
         StaticPopup_Show("DEATH");
     end)
 
-    urlFrame:Hide()
+    appealFrame:Hide()
 
-    return urlFrame
+    return appealFrame
 end
 
 local function addDeathPopupAppealButton()
-    local urlFrame = createUrlFrame()
+    local appealFrame = createAppealFrame()
 
     local deathOverride = function (data, reason)
         if (reason == "override") then
@@ -72,7 +71,7 @@ local function addDeathPopupAppealButton()
             return
         end
 
-        urlFrame:Show()
+        appealFrame:Show()
     end
 
     if (RETAIL == 1) then
